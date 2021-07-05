5d3bfdc660527a4667368b637ebf4eaefcef61d2
jung <ibory1220@naver.com>
2021-07-05 13:10:46 +0900

    JS : Navbar menu click

--- main.js
+++ main.js
@@ -11,3 +11,33 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  console.log("hi");
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
+
+const home__contact = document.querySelector(".home__contact");
+home__contact.addEventListener("click", () => {
+  const contact = document.querySelector("#contact");
+  contact.scrollIntoView({ behavior: "smooth" });
+});
+
+// Handle scrolling when tapping on the side button
+
+const pageBtn = document.querySelector(".pageBtn");
+pageBtn.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
