1435a84ea724ceee1560e2dfc058a91bc8526d84
jung <ibory1220@naver.com>
2021-07-09 16:50:25 +0900

    JS : Complete

--- main.js
+++ main.js
@@ -106,8 +106,75 @@ workBtnContainer.addEventListener("click", (event) => {
   }, 300);
 });
 
+// 1. 모든 섹션 요소들과 메뉴 아이템들을 가지고 온다.
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+
+// 관찰해야하는 모든 섹션요소들 가지고 온다.
+const sectionIds = [
+  "#home",
+  "#about",
+  "#skills",
+  "#work",
+  "#testimonials",
+  "#contact",
+];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map(
+  (id) => document.querySelector(`[data-key="${id}"]`) // 네비게이션 메뉴아이템 받아오기
+);
+
+const observerOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.3,
+};
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove("active");
+  selectedNavItem = selected; // 해당하는 navItem을 가져온다.
+  selectedNavItem.classList.add("active");
+}
+
 // scollIntoView
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      // 빠져나갈때(진입하지 않을때)
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        // 페이지가 내려가는 경우
+        selectedNavIndex = index - 1;
+      }
+    }
+  }); // 콜백안에서 해당하는 메뉴를 찾아서 navbar메뉴 활성화 시켜줘야한다.
+};
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener("wheel", () => {
+  // 사용자가 마우스나 트랙패드를 이용해서 스크롤하는 경우 발생
+  if (window.scrollY === 0) {
+    // 제일 위에있다면
+    selectedNavIndex = 0;
+  } else if (
+    Math.round(window.scrollY + window.innerHeight) >=
+    document.body.clientHeight
+  ) {
+    // 젤 밑으로 도달했다면. scrollY + 현재 윈도우창의 높이 === 전체적인 높이와 동일하다면
+    selectedNavIndex = navItems.length - 1; // 젤 마지막 인덱스를 가르킨다.
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+}); // 스크롤링이 될때마다 해당하는 메뉴 선택
